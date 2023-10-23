e46bd741fa57a78a8b57ec64ab51c8d95545f4f9
Dan Dectis <dandectis@gmail.com>
2023-10-23 12:40:15 -0700

    Standing this thing up

---
+++ assets/scripts/bubble.js
@@ -0,0 +1,45 @@
+const numberInput = document.getElementById('numbers-input')
+const executeButton = document.getElementById('execute-button')
+const resultsContainer = document.getElementById('results')
+
+//function to handle the swapping of values from index1 to index2
+const swap = (arr, index1, index2) => {
+    let temp = arr[index1]
+    arr[index1] = arr[index2]
+    arr[index2] = temp
+}
+
+const bubbleSort = (arr) => {
+    for (let i = 0; i < arr.length; i++){ //iterate over every value in the array
+        for (let index = 0; index < arr.length - i - 1; index++){ 
+            //In the inner loop, we subrtract i from the inner loop because by the time 
+            //the loop gets to the last item in the array, it will definitely be in its 
+            //correct position. Therefore, on the next pass through, we can omit that index value
+            //this has the effect of drastically reducing the number of iterations required
+            if (arr[index] > arr[index + 1]){
+                swap(arr, index, index + 1)
+            }
+        }
+    }
+
+    console.log("Sorted Array: " + arr)
+    if (arr.length > 0){
+        resultsContainer.innerText = "Sorted Number List: " + arr
+    } else {
+        resultsContainer.innerText = "No numbers were detected. Did you enter a comma separated list of numbers?"
+    }
+    
+
+}
+
+executeButton.addEventListener("click",function (){
+    let inputValue = numberInput.value;
+    if (!inputValue){
+        inputValue = '25,5,10,3,1,2';
+    }
+    
+    const inputArray = inputValue.split(",").map(Number)
+    const filteredArray = inputArray.filter(num => !isNaN(num))
+    console.log("Input Array: " + filteredArray)
+    bubbleSort(filteredArray);
+})
---
+++ assets/scripts/naive.js
@@ -0,0 +1,79 @@
+//create references to the places on the page that we will make use of later in the script
+const textBlockInput = document.getElementById("text-block-input");
+const searchTextInput = document.getElementById("text-search-input");
+const searchResultsHolder = document.getElementById("search-results");
+const caseSensitive = document.getElementById("case-sensitive");
+const searchButton = document.getElementById("search-button");
+const firstChunkHolder = document.getElementById('first-chunk');
+const answerHolder = document.getElementById('answer-chunk')
+const lastChunkHolder = document.getElementById('last-chunk')
+
+const pattern_search = (text, pattern, caseSensitive) =>{
+    console.log("Input Text: " + text + " Input Pattern: " + pattern);
+    let matchFound = false;
+    let results = "";
+    let firstChunk = "";
+    let answerChunk = "";
+    let lastChunk = "";
+    
+    
+    if (!caseSensitive){
+        text = text.toLowerCase();
+        pattern = pattern.toLowerCase();
+    }
+
+    for (let i = 0; i < text.length; i++){
+        console.log("Text Index: " + i);
+        let matchCount = 0 
+        for (let char = 0; char < pattern.length; char++){
+            console.log("Pattern Index: " + char)
+            if (pattern[char] === text[i + char]){
+                console.log("match found");
+                matchCount++
+            } else {
+                break
+            }
+        }
+        if (matchCount === pattern.length){
+            console.log(pattern + " found at index " + i)
+            results += "Found " + pattern + " at character " + i + ". "
+                                                                //split the input string into 3 chunks
+            firstChunk = text.substring(0,i)                    //this is the string chunk prior to the index that begins the answer
+            answerChunk = text.substring(i, i + pattern.length) //this is the found answer
+            lastChunk = text.substring(i + pattern.length)      //this is the remaining portion of the search block
+            matchFound = true;
+            
+        }
+    }
+
+    if (!matchFound){
+        console.error("Text searched. Match not found.")
+        results = "Text searched. Match not found."
+    }
+
+    searchResultsHolder.innerText = results
+    firstChunkHolder.innerText = "Searched: " + firstChunk;
+    answerHolder.innerText = answerChunk;
+    lastChunkHolder.innerText = lastChunk;
+    
+}
+
+searchButton.addEventListener("click", function() {
+    searchResultsHolder.innerText = "";
+    let text = textBlockInput.value;
+    let pattern = searchTextInput.value
+    console.log(textBlockInput.value)
+    if (!textBlockInput.value){
+        console.log("nothing entered")
+        text = "BNAAANBANABABANBANANANABANBBNAAANBANANBNAAANBANB"
+        
+    }
+    if (!searchTextInput.value){
+        pattern = "BANANA"
+    }
+    
+    pattern_search(text, pattern, caseSensitive.checked);
+})
+
+
+
